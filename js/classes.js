c6e4caf84b5d7dd418da4a535dba982800a038ea
Francis james <ibolokefrancisjames2@gmail.com>
2025-07-26 04:49:29 +0100

    added default classes for movies, users, review and some genre, then modified the script to accept es6 modules

---
+++ js/classes.js
@@ -0,0 +1,76 @@
+// movie class constructor
+export class Movie {
+    constructor(title, director, year, genre){
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.genre = genre;
+    }
+
+    display() {
+        return `${this.title} directed by ${this.director} (${this.year})`;
+      }
+}
+
+// user's class constructor
+export class User {
+    constructor(name){
+        this.name = name;
+        this.collection = []
+    }
+
+    addMovie(movie) {
+        this.collection.push(movie);
+      }
+}
+
+
+// Review class with private rating field
+export class Review {
+    #rating;
+
+    constructor(user, movie, rating, comment){
+        this.user = user;
+        this.movie = movie;
+        this.#rating = rating;
+        this.comment = comment
+    }
+
+    getRating() {
+        return this.#rating;
+      }
+    
+    display() {
+        return `${this.user.name} rated "${this.movie.title}" ${this.#rating} stars – "${this.comment}"`;
+      }
+}
+
+// 
+// 
+// 
+// Inherited classes for different genres
+
+// Action movie class
+export class ActionMovie extends Movie {
+    constructor(title, director, year, actionLevel){
+        super(title, director, year, 'Action')
+        this.actionLevel = actionLevel;
+    }
+
+    display() {
+        return `🔥 ACTION: ${this.title} - ${this.actionLevel} explosions!`;
+      }
+}
+
+
+// Comedy movie class
+export class ComedyMovie extends Movie{
+    constructor(title, director, year, humorLevel){
+        super(title, director, year, 'Comedy')
+        this.humorLevel = humorLevel;
+    }
+
+    display() {
+        return `😂 COMEDY: ${this.title} - ${this.humorLevel} jokes!`;
+      }
+}
