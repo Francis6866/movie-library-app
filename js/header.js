9a4d40ea5142eacdd6a7dc95bd68bb2a4c3e6521
Francis james <ibolokefrancisjames2@gmail.com>
2025-07-28 11:52:17 +0100

    Updated the main to also change between light and dark mode

--- js/header.js
+++ js/header.js
@@ -1,8 +1,10 @@
 export function toggleTheme() {
     const toggleBall = document.querySelector('.toggle-ball');
     const headerEl  = document.querySelector('.header')
+    const mainEl  = document.querySelector('.main')
 
     toggleBall.addEventListener('click', function(){
         headerEl.classList.toggle('light')
+        mainEl.classList.toggle('light')
     })
 }
